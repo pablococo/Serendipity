816b160df372c84e135c5c27194fe0a1ea81eaac
Pablo coco <coen11@naver.com>
2016-04-01 21:49:26 +0900

    Initial Commit

---
+++ Serendipity/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Serendipity
+//
+//  Created by Pablo coco on 2016. 4. 1..
+//  Copyright © 2016년 cocoplanners. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
