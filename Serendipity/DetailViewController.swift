242484373f064169954cb384f40ed5c322980771
Pablo coco <coen11@naver.com>
2016-04-04 18:03:20 +0900

    Hi there~

---
+++ Serendipity/DetailViewController.swift
@@ -0,0 +1,9 @@
+//
+//  DetailViewController.swift
+//  Serendipity
+//
+//  Created by Pablo coco on 2016. 4. 1..
+//  Copyright © 2016년 cocoplanners. All rights reserved.
+//
+
+import Foundation
---
+++ Serendipity/ListViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ListViewController.swift
+//  Serendipity
+//
+//  Created by Pablo coco on 2016. 4. 1..
+//  Copyright © 2016년 cocoplanners. All rights reserved.
+//
+
+import Foundation
