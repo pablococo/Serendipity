f0ff0693655f29dcae83f24023bc21f7f540b13e
Pablo coco <coen11@naver.com>
2016-04-04 18:06:09 +0900

    pink

--- Serendipity/AppDelegate.swift
+++ Serendipity/AppDelegate.swift
@@ -18,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         NSThread.sleepForTimeInterval(1.0);
         //start
+        //pink
         return true
     }
 
